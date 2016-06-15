bbd241d3d1b419982c1213ad139b660a66a2677d
Dlad <qdladoooo@gmail.com>
2016-06-15 14:48:34 +0800

    init

---
+++ readme.md
@@ -0,0 +1,57 @@
+##Sweeter-fetch for Laravel
+Wrapper for PDO.
+
+Procedure oriented, Pre process, As supplement of ORM.
+
+[![Latest Stable Version](https://poser.pugx.org/qdladoooo/sweeter-fetch/v/stable)](https://packagist.org/packages/qdladoooo/sweeter-fetch)
+[![Latest Unstable Version](https://poser.pugx.org/qdladoooo/sweeter-fetch/v/unstable)](https://packagist.org/packages/qdladoooo/sweeter-fetch)
+[![License](https://poser.pugx.org/qdladoooo/sweeter-fetch/license)](https://packagist.org/packages/qdladoooo/sweeter-fetch)
+##Installation
+```shell
+composer require qdladoooo/sweeter-fetch
+```
+##Initialization
+```php
+require "../vendor/autoload.php";
+use SweeterFetch\SweeterFetch;
+
+$sf = new SweeterFetch('host', 'username', 'password');
+```
+##Use
+Execute none query
+
+```php
+//return nothing
+$sf->Enq('use candy_shop;');
+```
+
+Execute query
+
+```php
+//return [row1, row2, ...]
+$sf->Eq($sql);
+```
+Execute one row
+
+```php
+//return the first row by array
+$sf->Eor($sql);
+```
+
+Execute column
+
+```php
+//return a column
+$sf->Ec($sql);
+```
+
+Execute scalar 
+
+```php
+//return a number
+$sf->Es($sql);
+```
+## License
+
+The Sweeter-fetch is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT).
+
---
+++ src/SweeterFetch.php
@@ -0,0 +1,77 @@
+<?php 
+namespace SweeterFetch;
+
+use Illuminate\Support\Facades\DB;
+class SweeterFetch {
+    protected static $pdo;
+    //init
+    function __construct( $connection_name = 'mysql' ) {
+        if (self::$pdo == null) {
+            self::$pdo = DB::connection( $connection_name )->getPdo();
+            self::$pdo->exec ( 'SET NAMES UTF8' );
+        }
+    }
+
+    //for dump
+    private function IsError() {
+        if( self::$need_dump_info ) {
+            $errorCode = self::$pdo->errorCode ();
+            if ($errorCode != '00000') {
+                var_dump ( self::$pdo->errorInfo () );
+                exit ();
+            }
+        }
+    }
+
+    //need dump info
+    function NeedDumpInfo( $flag = true ) {
+        self::$need_dump_info = (bool)$flag;
+    }
+
+    //execute query
+    function Eq($sql) {
+        $res = self::$pdo->query ( $sql );
+        $this->IsError ();
+        $res->setFetchMode ( \PDO::FETCH_ASSOC );
+        return $res->fetchAll ();
+    }
+
+    //execute one row
+    function Eor($sql) {
+        $res = self::$pdo->query ( $sql );
+        $this->IsError ();
+        $res->setFetchMode ( \PDO::FETCH_ASSOC );
+        $data = $res->fetch ();
+        return $data;
+    }
+
+    //execute col
+    function Ec($sql) {
+        $res = self::$pdo->query ( $sql );
+        $this->IsError ();
+        $res->setFetchMode ( \PDO::FETCH_ASSOC );
+        $data = $res->fetchAll ( \PDO::FETCH_COLUMN, 0 );
+        return $data;
+    }
+
+    //execute scalar
+    function Es($sql) {
+        $res = self::$pdo->query ( $sql );
+        $this->IsError ();
+        $res->setFetchMode ( \PDO::FETCH_NUM );
+        $data = $res->fetch ( \PDO::FETCH_COLUMN );
+        return $data;
+    }
+
+    //execute none query
+    function Enq($sql) {
+        $res = self::$pdo->exec($sql);
+        $this->IsError();
+
+        return $res;
+    }
+
+    function quote($var) {
+        return self::$pdo->quote ( $var );
+    }
+}
